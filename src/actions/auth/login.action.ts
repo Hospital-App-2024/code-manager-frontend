5f598b2c2034cd6bed72fddbe49ab39861a1eaa5
bryan lopez <blopez4434@gmail.com>
2024-06-21 04:52:43 -0400

    Update logout action to redirect to login page

--- src/actions/auth/login.action.ts
+++ src/actions/auth/login.action.ts
@@ -1,15 +1,30 @@
 "use server";
 import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import { LoginValues } from "@/schema/authSchema";
 
 export const loginAction = async (values: LoginValues) => {
   const formData = new FormData();
   formData.append("email", values.email);
   formData.append("password", values.password);
-  
-  await signIn("credentials", {
-    email: values.email,
-    password: values.password,
-  })
-  
+  try {
+    await signIn("credentials", {
+      redirectTo: `/`,
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
+  } catch (error) {
+    if (error instanceof AuthError)  {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return {
+            error: "Credenciales incorrectas",
+          };
+        default:
+          return { error: "Error desconocido" };
+      }
+    }
+
+    throw error;
+  }
 };
--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,37 @@
 import NextAuth from "next-auth"
 import { authConfig } from "./auth.config";
- 
+import Credentials from "next-auth/providers/credentials";
 export const {
-  handlers: { GET, POST },
+  auth,
   signIn,
   signOut,
-  auth,
+  handlers: { GET, POST },
 } = NextAuth({
   ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const { email, password } = credentials;
+
+        const res = await fetch(process.env.URL_BACKEND + "/auth/login", {
+          method: "POST",
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!res.ok) return null;
+
+        const user = await res.json();
+
+        return user;
+      },
+    }),
+  ],
 });
